63f51829132e64d406d102ec8e766d6225756800
Nikos <42405004+Nikolaos-Paximadakis@users.noreply.github.com>
2024-02-05 14:16:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,19 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+
 """
 # Welcome to Streamlit!
 
